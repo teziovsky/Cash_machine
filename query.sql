facfed10e5050489dc1f0cb5bb1683af3c83f46a
Teziovsky <teziovsky@gmail.com>
2019-12-27 17:58:11 +0100

    Dodanie pliku tworzącego bazę danych SQL
    Plik tworzący bazę danych, strukturę tabeli w bazie danych oraz wprowadza domyślne dane do tabeli.

---
+++ query.sql
@@ -0,0 +1,12 @@
+//Stworzenie bazy danych
+CREATE DATABASE IF NOT EXISTS Cash_machine;
+
+//Stworzenie tabeli cards
+CREATE TABLE IF NOT EXISTS Cards(
+    ID INTEGER NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+    Card_nr BIGINT NOT NULL UNIQUE,
+    Pass SMALLINT NOT NULL collate utf8_general_ci,
+    acc_balance DECIMAL(18, 2) NOT NULL)
+	
+//wprowadzenie danych do tabeli cards
+INSERT INTO cards VALUES (1, 123456789, 1234, 10000.00), (2, 987654321, 4321, 5000.00)
